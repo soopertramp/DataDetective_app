7841af1bf16aa13e118dc839dc2c8e6e2f01a76f
Pradeepchandra Reddy S C <pradeepchandrareddy95@gmail.com>
2023-06-30 12:56:38 +0530

    update

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
-""" CLEAN CODE """
-
 # Importing Necessary Libraries
+from utils import (apply_data_cleaning, basic_investigation,
+                   display_after_cleaning, display_download_count,
+                   generate_download_link, increment_download_count,
+                   initialize_download_count, plot_data, upload_file)
 
 import streamlit as st
-from utils import (initialize_download_count,
-                   increment_download_count, 
-                   upload_file,
-                   basic_investigation,
-                   apply_data_cleaning,
-                   display_after_cleaning,
-                   generate_download_link,
-                   plot_data,
-                   generate_report,
-                   display_download_count)
 
-# Set page title and layout
-st.set_page_config(page_title='Data Analysis App', layout='wide')
+# Set page title and layout 
+st.set_page_config(page_title="Analyst", layout='wide')
 
 # Main app
-def data_analysis_app() -> None:
+def data_analysis_app():
     """
     Run the Data Analysis App.
 
     This function runs the data analysis app, including uploading and generating a report, and displaying the download count.
-
-    Returns:
-        None.
-
     """
+    st.title("Hello, I'm A DataDetective :cop:")
+    st.write(" I'm here to help you analyze your data. Upload a CSV or Excel file, and I'll provide you with insights and investigation before diving into the data. Let's get started on your investigation and uncover valuable information from your data!")
     
-    st.title('Data Analysis App :chart_with_upwards_trend:')
-    
+    # Initialize download count
     download_count = initialize_download_count()
 
+    # Upload file and perform basic investigation
     df = upload_file()
     basic_investigation(df)
+
+    # Apply data cleaning and display cleaned data
     cleaned_df = apply_data_cleaning(df)
     display_after_cleaning(cleaned_df)
+
+    # Generate download link for cleaned dataset
     generate_download_link(cleaned_df, 'cleaned_dataset')
+
+    # Plot data
     plot_data(cleaned_df)
-        
-    generate_report(cleaned_df)
-    
+
+    # Increment download count
     download_count = increment_download_count()
-    
+
     # Display the download count
     display_download_count(download_count)  
 
